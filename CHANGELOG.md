4a6a90b0f43fa68e345fe761e02f8a945ef967ab
Bian Jiaping <ssbianjp@gmail.com>
2017-03-12 21:55:55 +0800

    Fix checking whether an asset exists in production environment

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## master
 
+## 0.1.1
+
+* Fix checking whether an asset exists in production environment
+
 ## 0.1.0
 
 The first release
--- app/helpers/easy_swagger_ui/application_helper.rb
+++ app/helpers/easy_swagger_ui/application_helper.rb
@@ -3,5 +3,13 @@ module EasySwaggerUi
     def swagger_routes
       Engine.routes.url_helpers
     end
+
+    def asset_available?(path)
+      if Rails.configuration.assets.compile
+        Rails.application.precompiled_assets.include?(path)
+      else
+        Rails.application.assets_manifest.assets[path].present?
+      end
+    end
   end
 end
